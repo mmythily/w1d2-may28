2aab1b7c64b875fe8ea86fd5cf708a89d36b799e
mmythily <mythily.mudunuru@gmail.com>
2019-05-28 12:01:10 -0400

    scoped and called statements - refactored

--- roll-dice.js
+++ roll-dice.js
@@ -1,16 +1,16 @@
 /*Write a program that takes a single parameter from the command line, 
 a number, and rolls that many six-sided dice. 
 */
-var numOfDice = process.argv.slice(2)[0];
 
-function rollDice (numOfDice) {
+function rollDice (num) {
     var arrayOfRolls = [];
-    for (dice = 0; dice <= numOfDice - 1; dice++) {
+    for (dice = 0; dice <= num - 1; dice++) {
         var roll = parseInt(Math.random() * 6) + 1;
         arrayOfRolls.push(roll)
     }
     var rollsInString = arrayOfRolls.join();
-    return `Rolled ${numOfDice} dice: ${rollsInString}`;
+    return `Rolled ${num} dice: ${rollsInString}`;
 }
 
+var numOfDice = process.argv.slice(2)[0];
 console.log(rollDice(numOfDice));
