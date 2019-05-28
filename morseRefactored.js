32399178ba1d39134fb8ee8de5a264f25ff99b27
mmythily <mythily.mudunuru@gmail.com>
2019-05-28 16:30:18 -0400

    morse code run through with Rafal

---
+++ morseRefactored.js
@@ -0,0 +1,33 @@
+var morseLookup = {
+    "s": "...",
+    "o": "---"
+  };
+  
+  var englishLookup = {
+    "...": "s",
+    "---": "o"
+  }
+  
+  function englishCharToMorseChar(char) {
+    return morseLookup[char.toLowerCase()];
+  };
+  
+  function morseCharToEnglishChar(morse) {
+    return englishLookup[morse.toLowerCase()];
+  };
+  
+  function mapChars(string, splitChar, fn, joinChar) {
+    return string.split(splitChar).map(fn).join(joinChar);
+  };
+  
+  function textToMorse(string) {
+    return mapChars(string, "", englishCharToMorseChar, " ");
+  };
+  
+  function morseToText (morseString) {
+    return mapChars(morseString, " ", morseCharToEnglishChar, "");
+  };
+  
+  console.log(textToMorse("SOS"));
+  
+  console.log(morseToText("... --- ..."));
